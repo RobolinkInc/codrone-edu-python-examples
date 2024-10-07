6ec5bfc6461c829dfec3d08d009812e2e892f503
dbautista2000 <112576370+dbautista2000@users.noreply.github.com>
2024-10-07 13:59:50 -0500

    Update reset_move_example.py

--- reset_move_example.py
+++ reset_move_example.py
@@ -8,7 +8,13 @@ drone.pair()
 drone.takeoff()
 drone.set_pitch(50)
 drone.set_roll(50)
+
+drone.move(2)
+
 drone.reset_move() # reset the pitch and roll to 0.
+
+drone.move(2) # after resetting flight variables, move(2) won't move the drone
+
 drone.land()
 
 
