379270c7f4e7b4cf4ffc529472c9c8b672658770
dbautista2000 <112576370+dbautista2000@users.noreply.github.com>
2024-11-05 10:52:45 -0800

    added python examples for 2.0 library release

---
+++ move_left_example.py
@@ -0,0 +1,15 @@
+#Python code
+from codrone_edu.drone import *
+import time
+
+drone = Drone()
+drone.pair()
+
+
+drone.takeoff()
+drone.move_left(distance=50, units="cm", speed=1)
+time.sleep(3) # make sure to add a delay so the drone has enough time to fly
+drone.land()
+
+
+drone.close()
