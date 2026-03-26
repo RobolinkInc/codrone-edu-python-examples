041f1140c8dc66488f3142a6d131696f449b77a2
dbautista2000 <112576370+dbautista2000@users.noreply.github.com>
2026-03-26 14:01:36 -0500

    Update drone connection and turn_right method

--- turn_right_example.py
+++ turn_right_example.py
@@ -2,14 +2,14 @@
 from codrone_edu.drone import *
 
 drone = Drone()
-drone.pair()
+drone.connect()
 
 
 drone.takeoff()
 drone.turn_right() # make a 90 degree right turn.
 drone.hover(1) # wait for 1 second in the air
-drone.turn_right(30, 3) # make a 30 degree right turn with a 3 second timeout.
+drone.turn_right(30) # make a 30 degree right turn.
 drone.land()
 
 
-drone.close()
+drone.disconnect()
